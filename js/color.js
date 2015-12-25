b19df7cb0d42efc0f3378cd67154b8d2c16ea397
Zhōu Bówēi 周伯威 <zhoubw12@mails.tsinghua.edu.cn>
2015-12-25 21:47:19 +0800

    add lab2rgb

--- js/color.js
+++ js/color.js
@@ -11,6 +11,7 @@ var Color = {
             RGB[i] = 100 * v;
         }
 
+        console.log(RGB);
         var X = RGB[0] * 0.4124 + RGB[1] * 0.3576 + RGB[2] * 0.1805;
         var Y = RGB[0] * 0.2126 + RGB[1] * 0.7152 + RGB[2] * 0.0722;
         var Z = RGB[0] * 0.0193 + RGB[1] * 0.1192 + RGB[2] * 0.9505;
@@ -18,7 +19,6 @@ var Color = {
         XYZ[0] /= 95.047;
         XYZ[1] /= 100.0;
         XYZ[2] /= 108.883;
-
         for (var i = 0; i < 3; i++) {
             var v = XYZ[i];
             if (v > 0.008856) {
@@ -35,5 +35,44 @@ var Color = {
         var b = 200 * (XYZ[1] - XYZ[2]);
         var Lab = [L, a, b];
         return Lab;
+    },
+    lab2rgb: function(inputColor) {
+        var L = inputColor[0];
+        var a = inputColor[1];
+        var b = inputColor[2];
+
+        var d = 6 / 29;
+        var fy = (L + 16) / 116;
+        var fx = fy + a / 500;
+        var fz = fy - b / 200;
+        var Y = fy > d ? fy * fy * fy : (fy - 16 / 116) * 3 * d * d;
+        var X = fx > d ? fx * fx * fx : (fx - 16 / 116) * 3 * d * d;
+        var Z = fz > d ? fz * fz * fz : (fz - 16 / 116) * 3 * d * d;
+
+        X *= 95.047;
+        Y *= 100.0;
+        Z *= 108.883;
+        // var R = 2.5623 * X + (-1.1661) * Y + (-0.3962) * Z;
+        // var G = (-1.0215) * X + 1.9778 * Y + 0.0437 * Z;
+        // var B = 0.0752 * X + (-0.2562) * Y + 1.1810 * Z;
+        var R = 3.2406 * X + (-1.5372) * Y + (-0.4986) * Z;
+        var G = (-0.9689) * X + 1.8758 * Y + 0.0415 * Z;
+        var B = 0.0557 * X + (-0.2040) * Y + 1.0570 * Z;
+
+        var RGB=[R,G,B];
+        console.log(RGB);
+        for (var i=0;i<3;i++){
+            var v=RGB[i]/100;
+            if (v>0.0405/12.92){
+                v=Math.pow(v,1/2.4);
+                v*=1.055;
+                v-=0.055;
+            }else{
+                v*=12.92;
+            }
+            RGB[i]=Math.round(v*255);
+        }
+
+        return RGB;
     }
 };
--- js/palette.js
+++ js/palette.js
@@ -7,18 +7,27 @@ Palette.init = function(img) {
     this.bin_size = 256 / this.bin_range;
     this.channels = 4;
     this.dataArray = img.data;
+    this.kmeans_iteration = 15;
+    this.K = 4;
+    this.kmeans_centers = [];
+
+    for (var i = 0; i < K + 1; i++) {
+        this.kmeans_centers.push({
+            color: null,
+            weight: 0,
+            acc: [0, 0, 0]
+        });
+    }
 
     for (var i = 0; i < this.bin_range; i++) {
         for (var j = 0; j < this.bin_range; j++) {
             for (var k = 0; k < this.bin_range; k++) {
-                this.bins['r' + i + 'g' + j + 'b' + k] = {
-                    color: {
-                        r: (i + 0.5) * this.bin_size,
-                        g: (j + 0.5) * this.bin_size,
-                        b: (k + 0.5) * this.bin_size
-                    },
+                var tmp = {
+                    color: [(i + 0.5) * this.bin_size, (j + 0.5) * this.bin_size, (k + 0.5) * this.bin_size],
                     count: 0
                 };
+                tmp.Lab = Color.rgb2lab(tmp.color);
+                this.bins['r' + i + 'g' + j + 'b' + k] = tmp;
             }
         }
     }
@@ -34,9 +43,21 @@ Palette.palette = function() {
         var gi = Math.floor(G / this.bin_size);
         var bi = Math.floor(B / this.bin_size);
         this.bins['r' + ri + 'g' + gi + 'b' + bi].count++;
-        if (this.bins['r' + ri + 'g' + gi + 'b' + bi].count == 10000){
+        if (this.bins['r' + ri + 'g' + gi + 'b' + bi].count == 10000) {
             console.log(this.bins['r' + ri + 'g' + gi + 'b' + bi].color);
         }
         // var Lab = Color.rgb2lab([R, G, B]);
     }
+}
+
+Palette.distance2 = function(c1, c2) {
+    var res = 0;
+    for (var i = 0; i < c1.length; i++) {
+        res += (c1[i] - c2[i]) * (c1[i] - c2[i]);
+    }
+    return res;
+}
+
+Palette.kmeans = function() {
+
 }
